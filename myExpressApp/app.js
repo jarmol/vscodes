fbeec275e02df794838ea7d5acd0c2a3fe29d339
Jarmo Lammi <jjlammi@yahoo.com>
2016-01-31 19:25:46 +0200

    Home page embedded here, reading data file

--- myExpressApp/app.js
+++ myExpressApp/app.js
@@ -1,33 +1,44 @@
 var express = require('express');
 
 var app = express();
-var path = __dirname + '/views/';
+var hbs = require('hbs');
+var blogEngine = require('./blog');
+
+app.set('view engine', 'html');
+app.engine('html', hbs.__express);
+
+// var path = __dirname + '/views/';
+// render knows default folder view, thus
+// path is not needed
 
 app.use('/javascripts', express.static(__dirname + '/views/js'));
 app.use('/stylesheets', express.static(__dirname + '/views/css'));
 app.use('/images', express.static(__dirname + '/views/images'));
 
 app.get('/', function(req, res) {
-    res.sendFile(path + 'index.html');
+   var x = blogEngine.getBlogEntries();
+
+   var otext = "<h1>Articles</h1>";
+   for (var i = 0; i < 6; i++) {
+    otext += "Id: " + x[i].id + ", Title: " + x[i].title + "<br>";
+   }
+   var foot = '<p><a href="/">Home</a> ~ <a href="/about">About Me</a> ~ <a href="/article">Some Article</a></p>';
+   res.send(otext + foot);
 });
  
 app.get('/about', function(req, res) {
-    res.sendFile(path + 'about.html');
+    res.render('about', {title: "About this project"});
 });
  
 app.get('/article', function(req, res) {
-    res.sendFile(path + 'article.html');
+    // var entry = blogEngine.getBlogEntry(req.params.id);
+    // res.render('article',{title:entry.title, blog:entry});
+    var x = blogEngine.getBlogEntries();
+    res.send(x[0]);
 });
  
 app.get('/api', function(request, response) {
     response.send({name:"Raymond",age:40});
-});
+})
 
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-app.listen(3000);
+app.listen(3000, function() {console.log('Listen port 3000');} )
